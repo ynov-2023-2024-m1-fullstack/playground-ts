9f42e1d20409c849c76fa66fd3ff5e1d1de60b2d
Vincent Zerbib <contact@zelabstudio.com>
2024-04-11 10:16:48 +0200

    sav work

--- dist/app.js
+++ dist/app.js
@@ -1,42 +1,62 @@
-var myName;
-var myAge;
-var hasPet;
-var ref;
-ref = 'AB1234';
-ref = 123456;
-ref = false;
-function getName(name) {
-    return "Your name is ".concat(name);
-    // return "Your name is" + name;
-}
-getName("Vincent");
-// getName(false);
-function logError(error) {
-    console.log(error);
-}
-logError("This is an error");
-var customer1 = {
+var user1 = {
+    name: "Vincent",
+    age: 33,
+    email: "vincenterbib@gmail.colm",
     firstName: "Vincent",
-    lastName: "Diaz",
+    lastName: "Zerbib"
 };
-var customer2 = {
+var user2 = {
+    name: "Vincent",
+    age: 33,
+    email: "vincenterbib@gmail.colm",
     firstName: "Vincent",
-    lastName: "Diaz",
-    age: 30
+    lastName: "Zerbib"
 };
-var Person = /** @class */ (function () {
-    function Person(age, firstName, lastName) {
+var UserCustomer = {
+    id: 1,
+    name: "Vincent",
+    age: 33,
+    email: 'vincentzerbib@gmail',
+    firstName: "Vincent",
+    lastName: "Zerbib",
+    username: "vzerbib"
+};
+var UserClass = /** @class */ (function () {
+    function UserClass(name, age) {
+        this.name = name;
+        this.age = age;
     }
-    return Person;
+    return UserClass;
 }());
-var user1 = new Person(33, "vincent", "zerbib");
-var product1 = {
-    name: "Soap",
-    price: 1.99,
-    salePrice: 0.99
-};
-function addToCard(product) {
-    console.log("Product added to cart: ", product);
+var Direction;
+(function (Direction) {
+    Direction[Direction["Top"] = 0] = "Top";
+    Direction[Direction["left"] = 1] = "left";
+    Direction[Direction["Right"] = 2] = "Right";
+    Direction[Direction["Bottom"] = 3] = "Bottom";
+})(Direction || (Direction = {}));
+var CodeStatus;
+(function (CodeStatus) {
+    CodeStatus[CodeStatus["OK"] = 200] = "OK";
+    CodeStatus[CodeStatus["Created"] = 201] = "Created";
+    CodeStatus[CodeStatus["NoContent"] = 204] = "NoContent";
+    CodeStatus[CodeStatus["NotModified"] = 304] = "NotModified";
+    CodeStatus[CodeStatus["BadRequest"] = 400] = "BadRequest";
+    CodeStatus[CodeStatus["Unauthorized"] = 401] = "Unauthorized";
+    CodeStatus[CodeStatus["Forbidden"] = 403] = "Forbidden";
+    CodeStatus[CodeStatus["NotFound"] = 404] = "NotFound";
+    CodeStatus[CodeStatus["InternalServerError"] = 500] = "InternalServerError";
+})(CodeStatus || (CodeStatus = {}));
+// function getUser(user: User) {
+//     console.log(user.name);
+// }
+function getUser(user) {
+    console.log(user.name);
 }
-addToCard(product1);
-addToCard(user1);
+function getUsers(users) {
+    users.forEach(function (user) {
+        console.log(user.name);
+    });
+}
+getUsers([user1, user2]);
+getUsers([1, 3, 4, 5, 6, 7, 8, 9, 10]);
--- src/app.ts
+++ src/app.ts
@@ -1,63 +1,98 @@
-let myName: string;
-let myAge: number | string;
-let hasPet: boolean;
-let ref : string | number | boolean | null;
-
-ref = 'AB1234';
-ref = 123456;
-ref = false;
-
-function getName(name:string): string {
-    return `Your name is ${name}`;
-    // return "Your name is" + name;
-}
-
-getName("Vincent");
-// getName(false);
-
-function logError(error: string): void {
-    console.log(error);
-}
-
-logError("This is an error");
-
-type Customer = {
+interface User {
+    name: string;
+    age: number;
+    email: string;
     firstName: string;
     lastName: string;
-    age?: number;
 }
 
-let customer1: Customer = {
+const user1: User = {
+    name: "Vincent",
+    age: 33,
+    email: "vincenterbib@gmail.colm",
     firstName: "Vincent",
-    lastName: "Diaz",
-};
-let customer2: Customer = {
+    lastName: "Zerbib"
+}
+const user2: User = {
+    name: "Vincent",
+    age: 33,
+    email: "vincenterbib@gmail.colm",
     firstName: "Vincent",
-    lastName: "Diaz",
-    age: 30
-};
-
-class Person {
-    constructor(age:number, firstName: string, lastName: string) {}
+    lastName: "Zerbib"
 }
 
-const user1: Person = new Person(33, "vincent", "zerbib");
+interface Customer extends User {
+    id: number;
+    username: string;
+}
+
+const UserCustomer: Customer = {
+    id: 1,
+    name: "Vincent",
+    age: 33,
+    email: 'vincentzerbib@gmail',
+    firstName: "Vincent",
+    lastName: "Zerbib",
+    username: "vzerbib"
+}
 
 interface Product {
-	name: string;
-	price : number;
-	salePrice : number;
+    name: string;
+    price: number;
 }
 
-const product1: Product = {
-    name: "Soap",
-    price: 1.99,
-    salePrice: 0.99
+interface Stock extends Product { 
+    stock: number;
+    instock: boolean,
 }
 
-function addToCard (product: Product) {
-    console.log("Product added to cart: ", product);
+class UserClass {
+    constructor(public name: string, public age: number) {}
 }
 
-addToCard(product1);
-addToCard(user1);
+enum Direction {
+    Top, 
+    left, 
+    Right, 
+    Bottom
+}
+
+enum CodeStatus {
+    OK = 200,
+    Created = 201,
+    NoContent = 204,
+    NotModified = 304,
+    BadRequest = 400,
+    Unauthorized = 401,
+    Forbidden = 403,
+    NotFound = 404,
+    InternalServerError = 500
+}
+
+interface Response {
+    code : CodeStatus;
+}
+
+interface Slider {
+    direction: Direction;
+}
+
+type IdUser = number
+
+// function getUser(user: User) {
+//     console.log(user.name);
+// }
+
+function getUser(user: User) {
+	console.log(user.name);
+}
+
+function getUsers(users: User[]) {
+    users.forEach(user => {
+        console.log(user.name);
+    });
+}
+
+getUsers([user1, user2]);
+
+getUsers([1, 3, 4, 5, 6, 7, 8, 9, 10]);
